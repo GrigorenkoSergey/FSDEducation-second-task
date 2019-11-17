c9d447b7e2d51eaba90c3d06e9a3de7be7900e56
Sergey Grigorenko <crazybuilder51@gmail.ru>
2019-11-17 08:08:23 +0300

    немного перенастроил webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,19 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, "./src"),
-  dist: path.join(__dirname, "./dist")
+  dist: path.join(__dirname, "./dist"),
 }
-
-const PAGES_DIR = `${PATHS.src}/pug/pages`;
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith(".pug"));
 
 let conf = {
   entry: {
-    main: `${PATHS.src}/index.js`,
+    index: `${PATHS.src}`,
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].js",
     publicPath: "",
   },
-  
+
   optimization: {
     splitChunks: {
       chunks: "all",
@@ -31,7 +28,7 @@ let conf = {
 
   devServer: {
     overlay: true,
-    port: 3000,
+    port: 8081,
   },
 
   module: {
@@ -39,69 +36,69 @@ let conf = {
       test: /\.js$/,
       loader: "babel-loader"
     },
-       {
-         test: /\.css$/,
-         use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-     },
-     {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          // Creates `style` nodes from JS strings
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-     },
-       {
-         test: /\.pug$/,
-         loader: "pug-loader"
-      },
-       {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/images"
-            }
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader"
+      ]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader",
+      ],
+    },
+    {
+      test: /\.pug$/,
+      use: [
+        {
+          loader: "pug-loader",
+        },
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "assets/images"
           }
-         ],
-       },
-       {
-         test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/fonts"
-            }
+        }
+      ],
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name]/[name].[ext]",
+            outputPath: "assets/fonts"
           }
-         ],
-       },
+        }
+      ],
+    },
     ]
   },
 
   plugins: [
     new CleanWebpackPlugin(),
 
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-         template: `${PAGES_DIR}/${page}`,
-         filename: `./${page.replace(/\.pug/, ".html")}`
-     })),
-
-    new MiniCssExtractPlugin({
-       filename: "assets/css/[name].css",
-       chunkFilename: "[id].css"
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.pug`,
+      filename: './index.html'
     }),
-  
+    
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+
     new CopyPlugin([
       { from: `${PATHS.src}/assets/images/`, to: 'assets/images/' },
     ]),
@@ -109,10 +106,10 @@ let conf = {
 };
 
 module.exports = (env, options) => {
-  let production = options.mode === "production"  ;
-  
+  let production = options.mode === "production";
+
   conf.devtool = production
-                  ? false  
-                  : "eval-sourcemap";
+    ? false
+    : "eval-sourcemap";
   return conf;
 }
