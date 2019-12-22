32dd438ddafaac8b7994fa8b629dbbb056803f45
GrigorenkoSergey <crazybuilder51@gmail.com>
2019-12-22 18:01:36 +0300

    доработал dropdown__input

--- src/pages/form_elements/form_elements.js
+++ src/pages/form_elements/form_elements.js
@@ -39,7 +39,6 @@ let dropdown = document.getElementsByClassName("dropdown");
 for (let item of dropdown) {
     let input = item.getElementsByClassName("dropdown__input")[0];
     let container = item.getElementsByClassName("dropdown__container")[0];
-    // let counter = item.getElementsByClassName("dropdown__counters")[0];
 
     input.addEventListener("click", function (e) {
         container.classList.toggle("dropdown__container_expanded");
@@ -48,19 +47,37 @@ for (let item of dropdown) {
 
     container.addEventListener("click", function(e) {
         if (!e.target.classList.contains("dropdown__circle")) return;   
-        let operation = e.target.dataset.direction;    
-           
+
+        let operation = e.target.dataset.direction;          
         let span = e.target.parentNode.querySelector("span");
         let value = +span.textContent;
+        let dataItem = span.dataset.item;
+
         if (operation == "+") {
-             span.textContent = ++value; 
+            if (value < 5) { //максимальное к-во элементов
+                span.textContent = ++value;                 
+            }
+
              let disabled = e.target.parentNode.querySelector(".dropdown__circle_disabled");
              if (disabled) disabled.classList.remove("dropdown__circle_disabled");   
         } else {
             span.textContent = Math.max(--value, 0);   
             if (value == 0) e.target.classList.add("dropdown__circle_disabled")         
-        }        
-    })
+        }  
+        inputContentRender();
+        
+    });
 
-    item.addEventListener("mousedown", (e) => e.preventDefault()); //убираем выделение        
+    item.addEventListener("mousedown", (e) => e.preventDefault()); //убираем выделение  
+
+    function inputContentRender() {
+        let roomsNum = +container.querySelector("span[data-item = 'rooms_num']").textContent;
+        let bedsNum = +container.querySelector("span[data-item = 'beds_num']").textContent;
+        let bathroomsNum = +container.querySelector("span[data-item = 'bathrooms_num']").textContent;
+
+        let roomsEnds = ["ен", "ьня", "ьни", "ьни", "ьни", "ен"];
+        let bedsEnds = ["ей", "ь", "и", "и", "и", "ей"];
+
+        input.textContent = `${roomsNum} спал${roomsEnds[roomsNum]}, ${bedsNum} кроват${bedsEnds[bedsNum]}...`; 
+    }
 }
