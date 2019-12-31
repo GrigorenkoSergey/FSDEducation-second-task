0593f94e63e5895fd9ac6840dd97e31cd9f90499
GrigorenkoSergey <crazybuilder51@gmail.com>
2019-12-31 08:02:35 +0300

    Начал разделять блоки на файлы

--- webpack.config.js
+++ webpack.config.js
@@ -127,6 +127,7 @@ let conf = {
 
     new CopyPlugin([
       { from: `${PATHS.src}/assets/images/`, to: `${PATHS.dist}/assets/images/` },
+      { from: `${PATHS.src}/assets/blocks/`, to: `${PATHS.dist}/assets/blocks/` },
       { from: `${PATHS.src}/assets/fonts/`, to: `${PATHS.dist}/assets/fonts/` },
     ]),
   ],
