5f3d406b239d6cc9e0e50b4afcc7c2e896814d84
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-01-06 09:19:26 +0300

    Начал делать UI kit Cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 //Файлы индекса использовал исключительно для отладки и верстки каждой страницы
-import "./pages/form_elements/form_elements.js";
+
 // import "./pages/colors_&_type/colors_&_type.js";
+// import "./pages/form_elements/form_elements.js";
+import "./pages/cards/cards.js";
