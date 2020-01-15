8e090a19a69943e26023458682ca3de7f7d57fb7
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-01-15 04:19:32 +0300

    Доделал форму бронирования

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -6,13 +6,18 @@ import "../../assets/blocks/final-check/final-check.js";
 let arrivals = dateDropdowns.arrivals;
 let departures = dateDropdowns.departures;
 
-let arrival = arrivals.filter(item => item.el.dataset.name == "arrival_2")[0];
+let arrival = arrivals.filter(item => item.el.dataset.name == "arrival_calendar")[0];
 arrival.el.classList.add("_invisible");
 arrival.alwaysShow = true;
 arrival.show();
 
-let departure = departures.filter(item => item.el.dataset.name == "departure_2")[0];
+let departure = departures.filter(item => item.el.dataset.name == "departure_calendar")[0];
 departure.el.classList.add("_invisible");
 
 arrival.setDate(new Date(2019, 7, 19), true)
 departure.setDate(new Date(2019, 7, 23), true)
+
+arrival = arrivals.filter(item => item.el.dataset.name == "arrival_final-check")[0];
+departure = departures.filter(item => item.el.dataset.name == "departure_final-check")[0];
+arrival.setDate(new Date(2019, 7, 19));
+departure.setDate(new Date(2019, 7, 23));
