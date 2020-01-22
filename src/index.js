28ff482565a3ed6368d8251226f294d7fcd9100b
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-01-22 16:37:46 +0300

    Закончил footer-widgets && copyright

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //Файлы индекса использовал исключительно для отладки и верстки каждой страницы
 
 // import "./pages/colors_&_type/colors_&_type.js";
-import "./pages/form_elements/form_elements.js";
+// import "./pages/form_elements/form_elements.js";
 // import "./pages/cards/cards.js";
-// import "./pages/headers_and_footers/headers_and_footers.js";
+import "./pages/headers_and_footers/headers_and_footers.js";
--- src/pages/headers_and_footers/headers_and_footers.js
+++ src/pages/headers_and_footers/headers_and_footers.js
@@ -1,2 +1,3 @@
 import "./headers_and_footers.scss";
+import "../../assets/blocks/footer/footer.js";
 // import "../../assets/blocks/search-form/search-form.js";
