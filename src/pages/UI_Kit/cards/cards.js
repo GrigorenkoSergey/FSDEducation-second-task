dba998116530270daca93493c6a3e1cb02b37042
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-02-21 03:51:35 +0300

    Закончил room-details

--- src/pages/UI_Kit/cards/cards.js
+++ src/pages/UI_Kit/cards/cards.js
@@ -1,7 +1,7 @@
 import "./cards.scss";
 import "../../../assets/blocks/search-form/search-form.js";
 import dateDropdowns from "../../../assets/blocks/date-dropdown/date-dropdown.js";
-import "../../../assets/blocks/final-check/final-check.js";
+import "../../../assets/blocks/final-bill/final-bill.js";
 import "../../../assets/blocks/registration/registration.js";
 import "../../../assets/blocks/log-in/log-in.js";
 import "../../../assets/blocks/preview/preview.js";
@@ -20,7 +20,7 @@ departure.el.classList.add("_invisible");
 arrival.setDate(new Date(2019, 7, 19), true)
 departure.setDate(new Date(2019, 7, 23), true)
 
-arrival = arrivals.filter(item => item.el.dataset.name == "arrival_final-check")[0];
-departure = departures.filter(item => item.el.dataset.name == "departure_final-check")[0];
+arrival = arrivals.filter(item => item.el.dataset.name == "arrival_final-bill")[0];
+departure = departures.filter(item => item.el.dataset.name == "departure_final-bill")[0];
 arrival.setDate(new Date(2019, 7, 19));
 departure.setDate(new Date(2019, 7, 23));
--- src/pages/Website_pages/room_details/room_details.js
+++ src/pages/Website_pages/room_details/room_details.js
@@ -1,13 +1,13 @@
 import "./room_details.scss";
 import dateDropdowns from "../../../assets/blocks/date-dropdown/date-dropdown.js";
 import "../../../assets/blocks/comment/comment.js";
-import "../../../assets/blocks/final-check/final-check.js";
+import "../../../assets/blocks/final-bill/final-bill.js";
 import "../../../assets/blocks/diagram/diagram.js";
 
 let arrivals = dateDropdowns.arrivals;
 let departures = dateDropdowns.departures;
 
-let arrival = arrivals.filter(item => item.el.dataset.name == "arrival_final-check")[0];
-let departure = departures.filter(item => item.el.dataset.name == "departure_final-check")[0];
+let arrival = arrivals.filter(item => item.el.dataset.name == "arrival_final-bill")[0];
+let departure = departures.filter(item => item.el.dataset.name == "departure_final-bill")[0];
 arrival.setDate(new Date(2019, 7, 19));
 departure.setDate(new Date(2019, 7, 23));
