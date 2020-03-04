427a3c24e165a5b5a0f51327a8b84979ff899714
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-03-04 18:35:57 +0300

    Закончил search room

--- src/pages/Website_pages/search_room/search_room.js
+++ src/pages/Website_pages/search_room/search_room.js
@@ -2,3 +2,4 @@ import "./search_room.scss";
 import "../../../assets/blocks/dropdown/dropdown.js";
 import "../../../assets/blocks/range-slider/range-slider.js";
 import "../../../assets/blocks/checkboxes/checkboxes.js";
+import "../../../assets/blocks/preview/preview.js";
