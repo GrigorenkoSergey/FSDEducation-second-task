a0935c6dc9236fd99e350de4f1b6171bfc25e31e
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-05-24 07:40:51 +0300

    Внес дофига исправлений, в основном в путях scss файлов, а также в конфигах вебпака

---
+++ webpack.dev.config.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, "./src"),
+  dist: path.join(__dirname, "./dist"),
+  favicons: path.join(__dirname, "./favicons"),
+}
+
+module.exports = {
+  entry: `${PATHS.src}/index`, 
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+    filename: '[name].js',
+  },
+
+  devServer: {
+    overlay: true,
+    port: 3000,
+  },
+
+  devtool: "inline-source-map",
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: "babel-loader"
+    },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+      ]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: {
+            url: false, //супер строка, решила проблемы с поиском assets от корня сайта
+            // эксперименты с resolve-url-loader ни к чему не привели. Потерял целый день.
+          },
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: true,
+          }
+        }
+      ],
+    },
+    {
+      test: /\.pug$/,
+      use: [
+        {
+          loader: "pug-loader",
+        }
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    },
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.pug`,
+      filename: './index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+
+    new CopyPlugin([
+      { from: `${PATHS.src}/assets/blocks/`, to: `${PATHS.dist}/assets/blocks/` },
+      { from: `${PATHS.src}/assets/fonts/`, to: `${PATHS.dist}/assets/fonts/` },
+      { from: `${PATHS.src}/assets/images/`, to: `${PATHS.dist}/assets/images` },
+      { from: `${PATHS.favicons}/`, to: `${PATHS.dist}/favicons/` },
+    ]),
+  ],
+};
