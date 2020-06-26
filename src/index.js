652eaa646dae67188ffc80f0f8e77b10367abe0b
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-06-26 04:45:14 +0300

    Переименовал файлы шрифтов и названия шрифтов в проекте по стилю lower-case-hyphenated

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// import "./pages/UI_Kit/cards/cards.js";
-// import "./pages/UI_Kit/colors_and_type/colors_and_type.js";
-// import "./pages/UI_Kit/form_elements/form_elements.js";
-// import "./pages/UI_Kit/headers_and_footers/headers_and_footers.js";
-// import "./pages/Website_pages/sign_in/sign_in.js";
-// import "./pages/Website_pages/registration/registration.js";
-// import "./pages/Website_pages/room_details/room_details.js";
-// import "./pages/Website_pages/search_room/search_room.js";
-// import "./pages/Website_pages/landing_page/landing_page.js";
