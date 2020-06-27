0e3cded24a6b8081d955b9c4b39c5f9c508b0574
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-06-27 04:55:21 +0300

    Разделил checkboxes

--- src/pages/Website_pages/search_room/search_room.js
+++ src/pages/Website_pages/search_room/search_room.js
@@ -1,5 +1,5 @@
 import "./search_room.scss";
 import "../../../assets/blocks/dropdown/dropdown.js";
 import "../../../assets/blocks/range-slider/range-slider.js";
-import "../../../assets/blocks/checkboxes/checkboxes.js";
 import "../../../assets/blocks/preview/preview.js";
+import "../../../assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js";
