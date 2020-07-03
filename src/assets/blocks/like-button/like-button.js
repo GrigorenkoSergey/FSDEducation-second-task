ab4a985ccc0c054222129903e48abdb8cd0a7a19
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-07-03 05:19:10 +0300

    Переделал like-button и rate-button

--- src/assets/blocks/like-button/like-button.js
+++ src/assets/blocks/like-button/like-button.js
@@ -1,15 +1,23 @@
-let likeButtons = document.getElementsByClassName("like-button");
-for (let item of likeButtons) {
-  item.addEventListener("click", function(e) {
-    this.classList.toggle("like-button_pushed");
+class LikeButton {
+  constructor(item) {
+    this.el = item;
+    this.counter = item.querySelector(".like-button__counter");
+    this.el.addEventListener("click", this.onClick.bind(this));
+  }
 
-    let counter = this.getElementsByClassName("like-button__counter")[0];
+  onClick(e) {
+    let {el, counter} = this;
 
-    if (this.classList.contains("like-button_pushed")) {
-
+    el.classList.toggle("like-button_pushed");
+    if (el.classList.contains("like-button_pushed")) {
       counter.textContent = +(counter.textContent) + 1;
     } else {
       counter.textContent = +(counter.textContent) - 1;
     }
-  })
+  }
+}
+
+let likeButtons = document.getElementsByClassName("like-button");
+for (let item of likeButtons) {
+  new LikeButton(item);
 }
--- src/assets/blocks/rate-button/rate-button.js
+++ src/assets/blocks/rate-button/rate-button.js
@@ -1,13 +1,24 @@
-let rateButtons = document.getElementsByClassName("rate-button");
-for (let item of rateButtons) {
-  item.addEventListener("click", function(e) {
+class RateButton {
+  constructor(item) {
+    this.el = item;
+    this.el.addEventListener("click", this.onClick.bind(this));
+  }
+
+  onClick(e) {
     let stars = +e.target.dataset.star;
+
     for (let i = 0; i < 5; i++) {
       if (i < stars) {
-        item.children[i].className = "rate-button__star_checked"
+        this.el.children[i].className = "rate-button__star_checked"
       } else {
-        item.children[i].className = "rate-button__star"
+        this.el.children[i].className = "rate-button__star"
       }
     }
-  })
+  }
+}
+
+let rateButtons = document.getElementsByClassName("rate-button");
+
+for (let item of rateButtons) {
+  new RateButton(item);
 }
