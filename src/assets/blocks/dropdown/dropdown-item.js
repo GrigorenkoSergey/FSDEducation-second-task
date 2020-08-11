5e6ae879117590de9990907f94aa39791c8c5218
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-08-11 10:08:40 +0300

    Убрал везде где возможно абсолютное позиционирование

--- src/assets/blocks/dropdown/dropdown-item.js
+++ src/assets/blocks/dropdown/dropdown-item.js
@@ -18,6 +18,17 @@ export default class DropdownItem extends EventObserver {
 
     this.minus.addEventListener('click', this.handleMinusClick.bind(this));
     this.plus.addEventListener('click', this.handlePlusClick.bind(this));
+    this.update();
+  }
+
+  update() {
+    if (this.value <= MIN_ITEMS_VALUE) {
+      this.minus.classList.add('dropdown__minus_disabled');
+    }
+
+    if (this.value >= MAX_ITEMS_VALUE) {
+      this.plus.classList.add('dropdown__plus_disabled');
+    }
   }
 
   handleMinusClick(e) {
@@ -30,10 +41,7 @@ export default class DropdownItem extends EventObserver {
     this.value -= 1;
     this.counter.textContent = this.value;
     this.broadcast('changeItemValue', this);
-
-    if (this.value === MIN_ITEMS_VALUE) {
-      this.minus.classList.add('dropdown__minus_disabled');
-    }
+    this.update();
   }
 
   handlePlusClick(e) {
@@ -46,9 +54,6 @@ export default class DropdownItem extends EventObserver {
     this.value += 1;
     this.counter.textContent = this.value;
     this.broadcast('changeItemValue', this);
-
-    if (this.value === MAX_ITEMS_VALUE) {
-      this.plus.classList.add('dropdown__plus_disabled');
-    }
+    this.update();
   }
 }
