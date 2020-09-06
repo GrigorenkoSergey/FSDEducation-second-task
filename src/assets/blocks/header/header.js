8d6c903128dc0b39519dfa6dc6cae83d485f1182
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-09-06 20:42:17 +0300

    Переделал хедер с учетом адаптивности

--- src/assets/blocks/header/header.js
+++ src/assets/blocks/header/header.js
@@ -1,6 +1,40 @@
+/* eslint-disable class-methods-use-this */
 import './header.scss';
 
 import '../button/button.js';
 import '../footer/footer.js';
 import '../copyright/copyright.js';
 import '../footer-creative/footer-creative.js';
+
+import HeaderSubmenu from './header__submenu.js';
+
+class Header {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.el = document.querySelector('.header');
+    this.trigger = this.el.querySelector('.header__trigger');
+    this.nav = this.el.querySelector('.header__nav');
+    this.buttons = this.el.querySelector('.header__buttons');
+    this.userLabel = this.el.querySelector('.header__user-name');
+
+    [...this.el.querySelectorAll('.header__submenu-title')]
+      .map((item) => new HeaderSubmenu(item.parentNode));
+
+    this.trigger.addEventListener('click', this.handleTriggerClick.bind(this));
+  }
+
+  handleTriggerClick(e) {
+    this.nav.classList.toggle('header__nav_visible');
+
+    if (this.buttons) {
+      this.buttons.classList.toggle('header__buttons_visible');
+    } else {
+      this.userLabel.classList.toggle('header__user-name_visible');
+    }
+  }
+}
+
+const header = new Header();
---
+++ src/assets/blocks/header/header__submenu.js
@@ -0,0 +1,36 @@
+export default class HeaderSubmenu {
+  constructor(el) {
+    this.el = el;
+    this.handlers = {};
+    this.init();
+  }
+
+  bindHandlers() {
+    this.handlers.handleTitleClick = this.handleTitleClick.bind(this);
+    this.handlers.handleDocumentClick = this.handleDocumentClick.bind(this);
+  }
+
+  init() {
+    this.bindHandlers();
+
+    this.title = this.el.querySelector('.header__submenu-title');
+    this.menu = this.el.querySelector('.header__submenu');
+
+    this.title.addEventListener('click', this.handlers.handleTitleClick);
+  }
+
+  handleTitleClick(e) {
+    this.menu.classList.toggle('header__submenu_visible');
+    this.title.classList.add('header__submenu-title_hovered');
+
+    document.addEventListener('click', this.handlers.handleDocumentClick);
+  }
+
+  handleDocumentClick(e) {
+    if (!this.el.contains(e.target)) {
+      document.removeEventListener('click', this.handlers.handleDocumentClick);
+      this.menu.classList.remove('header__submenu_visible');
+      this.title.classList.remove('header__submenu-title_hovered');
+    }
+  }
+}
