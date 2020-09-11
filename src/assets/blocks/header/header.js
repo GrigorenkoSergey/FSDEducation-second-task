84dc907d2833b146c5d4a8af985a64ea8753c9ec
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-09-11 17:28:20 +0300

    Добавил скрытия меню хедера при щелчке вне триггера

--- src/assets/blocks/header/header.js
+++ src/assets/blocks/header/header.js
@@ -8,10 +8,19 @@ import HeaderSubmenu from './header__submenu.js';
 
 class Header {
   constructor() {
+    this.handlers = {};
+    this.menuExpanded = false;
     this.init();
   }
 
+  bindHandlers() {
+    this.handlers.handleDocumentClick = this.handleDocumentClick.bind(this);
+    this.handlers.handleTriggerClick = this.handleTriggerClick.bind(this);
+  }
+
   init() {
+    this.bindHandlers();
+
     this.el = document.querySelector('.header');
     this.trigger = this.el.querySelector('.header__trigger');
     this.menu = this.el.querySelector('.header__menu');
@@ -19,11 +28,25 @@ class Header {
     [...this.el.querySelectorAll('.header__submenu-title')]
       .map((item) => new HeaderSubmenu(item.parentNode));
 
-    this.trigger.addEventListener('click', this.handleTriggerClick.bind(this));
+    this.trigger.addEventListener('click', this.handlers.handleTriggerClick);
   }
 
   handleTriggerClick(e) {
     this.menu.classList.toggle('header__menu_visible');
+    this.menuExpanded = !this.menuExpanded;
+
+    if (this.menuExpanded) {
+      document.addEventListener('click', this.handlers.handleDocumentClick);
+    }
+  }
+
+  handleDocumentClick(e) {
+    if (this.el.contains(e.target)) return;
+
+    this.menu.classList.remove('header__menu_visible');
+    this.menuExpanded = false;
+
+    document.removeEventListener('click', this.handlers.handleDocumentClick);
   }
 }
 
